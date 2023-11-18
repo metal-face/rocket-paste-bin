748778932bbc155068dd4f2a6ed8f61042aca302
metal-face <hughesbryan3000@gmail.com>
2023-11-18 00:08:11 -0500

    from param function written

--- src/paste_id.rs
+++ src/paste_id.rs
@@ -1,9 +1,11 @@
 use std::borrow::Cow;
 use std::path::{Path, PathBuf};
 
+use rocket::request::FromParam;
 use rand::{self, Rng};
 use rand::rngs::ThreadRng;
 
+#[derive(UriDisplayPath)]
 pub struct PasteId<'a>(Cow<'a, str>);
 
 impl PasteId<'_> {
@@ -23,5 +25,15 @@ impl PasteId<'_> {
     pub fn file_path(&self) -> PathBuf {
         let root = concat!(env!("CARGO_MANIFEST_DIR"), "/", "upload");
         Path::new(root).join(self.0.as_ref())
+    }
+}
+
+impl<'a> FromParam<'a> for PasteId<'a> {
+    type Error = &'a str;
+
+    fn from_param(param: &'a str) -> Result<Self, Self::Error> {
+        param.chars().all(|c: char| c.is_ascii_alphanumeric())
+            .then(|| PasteId(param.into()))
+            .ok_or(param)
     }
 }
