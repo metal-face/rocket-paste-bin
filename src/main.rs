22b2d473f6e4c8f272683f07cd3723ca672e67cb
metal-face <hughesbryan3000@gmail.com>
2023-11-18 00:08:34 -0500

    retrieve and upload routes written

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 mod paste_id;
 
+use std::path::{Path, PathBuf};
+use rocket::data::{Data, ToByteUnit};
+use rocket::http::uri::Absolute;
+use rocket::tokio::fs::File;
+use rocket::uri;
+use paste_id::PasteId;
+
 #[macro_use] extern crate rocket;
 
 #[get("/")]
@@ -18,7 +25,25 @@ fn index() -> &'static str {
     "
 }
 
+#[get("/<id>")]
+async fn retrieve(id: PasteId<'_>) -> Option<File> {
+    File::open(id.file_path()).await.ok()
+}
+
+
+const ID_LENGTH: usize = 3;
+const HOST: Absolute<'static> = uri!("http://localhost:8000");
+
+#[post("/", data = "<paste>")]
+async fn upload(paste: Data<'_>) -> std::io::Result<String> {
+    let paste_id: PasteId = PasteId::new(ID_LENGTH);
+
+    paste.open(128.kibibytes()).into_file(paste_id.file_path()).await?;
+
+    Ok(uri!(HOST, retrieve(paste_id)).to_string())
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, retrieve, upload])
 }
